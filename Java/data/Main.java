4d21a7c233ac9c9a580c854b6f57347e07fcda31
Nguyen Trung Tin <trztinn@gmail.com>
2026-09-24 10:46:21 +0700

    Nộp bài Lab2

--- Java/data/Main.java
+++ Java/data/Main.java
@@ -2,8 +2,6 @@ package data;
 
 public class Main {
    public static void main(String[] args) {
-        Database data = new MySQL();
-        OrderService sv1 = new OrderService(data);
-        sv1.checkout("0792252181");
+        
     }
 }
